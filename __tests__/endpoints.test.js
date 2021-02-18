595333076074d506543c54dce3fb1358db6473b8
Vance-M <myveryvills@yahoo.com>
2021-02-18 15:50:27 -0800

    testing

--- __tests__/endpoints.test.js
+++ __tests__/endpoints.test.js
@@ -1,20 +1,11 @@
 const { app } = require('../app.js');
-// const { pokemon } = require('../data.js');
+const { doughnuts } = require('../donuts.js');
 const supertest = require('supertest');
 const request = supertest(app);
 
-it('gets the test endpoint', async done => {
-    const myData = [
-        {
-            id: 2,
-            name: 'spot'
-        },
-        {
-            id: 3,
-            name: 'jeep'
-        }
-    ];
-    const response = await request.get('/test');
+it('should respond with the doughnuts from my list', async done => {
+    const myData = { results: doughnuts };
+    const response = await request.get('/donuts');
   
     expect(response.status).toBe(200);
     expect(response.body.message).toEqual(myData);
