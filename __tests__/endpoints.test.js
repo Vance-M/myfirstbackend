b4475f6205e660c086593e7c5884884fbd54f3a2
Vance-M <myveryvills@yahoo.com>
2021-02-18 16:08:11 -0800

    test 2

--- __tests__/endpoints.test.js
+++ __tests__/endpoints.test.js
@@ -12,3 +12,21 @@ it('should respond with the doughnuts from my list', async done => {
     expect(response.body).toEqual(myData);
     done();
 });
+
+it('should respond with the doughnuts from my list', async done => {
+    const myData = { results:{
+        'description': 'Doughnut made with sour cream and a tapered edge',
+        'id': 2,
+        'image': 'old-fashioned.jpeg',
+        'name': 'Old-Fashioned',
+        'price': '.60',
+        'specialty': false,
+    }
+    };
+
+    const response = await request.get('/doughnuts/2');
+  
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual(myData);
+    done();
+});
