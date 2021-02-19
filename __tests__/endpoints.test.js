c6820c478bdbb0dae0920c72b07221240b0b6145
Vance-M <myveryvills@yahoo.com>
2021-02-18 16:04:01 -0800

    1 test down

--- __tests__/endpoints.test.js
+++ __tests__/endpoints.test.js
@@ -1,13 +1,14 @@
 const { app } = require('../app.js');
-const { doughnuts } = require('../donuts.js');
 const supertest = require('supertest');
 const request = supertest(app);
+const { doughnuts } = require('../donuts.js');
 
 it('should respond with the doughnuts from my list', async done => {
     const myData = { results: doughnuts };
-    const response = await request.get('/donuts');
+
+    const response = await request.get('/doughnuts');
   
     expect(response.status).toBe(200);
-    expect(response.body.message).toEqual(myData);
+    expect(response.body).toEqual(myData);
     done();
 });
